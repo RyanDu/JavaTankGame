11a020ed31d821739aef0460c4bef2d6548e079d
RyanDu <38671873+RyanDu@users.noreply.github.com>
2019-09-03 16:14:26 -0500

    Add enemy tank

--- src/main/java/javaProject/tankWar/GameClient.java
+++ src/main/java/javaProject/tankWar/GameClient.java
@@ -4,19 +4,34 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameClient extends JComponent {
 
     private Tank playerTank;
 
-    public GameClient(){
-        this.playerTank = new Tank(400,100,Direction.DOWN);
-        this.setPreferredSize(new Dimension(800,600));
+    private List<Tank> enemyTank;
+
+    private List<Walls> walls;
+
+    public GameClient() {
+        this.playerTank = new Tank(400, 100, Direction.DOWN);
+        this.enemyTank = new ArrayList<>(12);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                this.enemyTank.add(new Tank(200 + j * 120, 400 + i * 40, true, Direction.UP));
+            }
+        }
+        this.setPreferredSize(new Dimension(800, 600));
     }
 
     @Override
     public void paintComponent(Graphics g) {
         playerTank.draw(g);
+        for(Tank tank: enemyTank){
+            tank.draw(g);
+        }
     }
 
     public static void main(String[] args) {
@@ -39,6 +54,7 @@ public class GameClient extends JComponent {
             public void keyPressed(KeyEvent e) {
                 client.playerTank.keyPressed(e);
             }
+
             @Override
             public void keyReleased(KeyEvent e) {
 
@@ -47,12 +63,12 @@ public class GameClient extends JComponent {
             }
         });
         frame.setVisible(true);
-
-        while(true){
+//
+        while (true) {
             client.repaint();
-            try{
+            try {
                 Thread.sleep(50);
-            }catch (InterruptedException e){
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
